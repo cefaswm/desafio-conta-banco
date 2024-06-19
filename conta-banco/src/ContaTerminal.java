180995baa9395b5a9596595611e694ec5fb42c6d
Cefas Willy <cefasnegocios12@gmail.com>
2024-06-18 22:28:11 -0300

    Docs: update desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o numero da sua conta: EX: 0000 ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite o numero da sua agencia: EX: 000-0 ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu usuario: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu saldo atual ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ". Obrigado por criar uma conta em nosso banco,sua agência é " + agencia
+                + ", conta " + conta + " e seu saldo " + saldo + " ja está disponivel para saque.");
+
     }
 }
